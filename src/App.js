3366451c0c56438c0dcf984e14f718cb24655261
fanifrancs <fanikufran6@gmail.com>
2022-07-05 03:36:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,98 +1,66 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function BoilingVerdict(props) {
   if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
+    return <p>Water would <span className='boil'>boil</span>.</p>;
   }
-  return <p>The water would not boil.</p>;
+  return <p>Water would <span className='not-boil'>not boil</span>.</p>;
 }
 
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit'
-};
-
 function toCelsius(fahrenheit) {
+  if (fahrenheit === '') {
+    return '';
+  }
   return (fahrenheit - 32) * 5 / 9;
 }
 
 function toFahrenheit(celsius) {
+  if (celsius === '') {
+    return '';
+  }
   return (celsius * 9 / 5) + 32;
 }
 
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
+function TemperatureInput(props) {
+  return (
+    <fieldset>
+      <legend>Enter temperature in {props.scale}</legend>
+      <input value={props.temp} onChange={props.onTemperatureChange} />
+    </fieldset>
+  );
 }
 
-class TemperatureInput extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
+export default function Converter() {
+  const [CTemp, setCTemp] = useState();
+  const [FTemp, setFTemp] = useState();
+
+  function handleCelsiusChange(e) {
+    let input = e.target.value;
+    let result = toFahrenheit(input);
+    setCTemp(input); setFTemp(result);
   }
 
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
+  function handleFahrenheitChange(e) {
+    let input = e.target.value;
+    let result = toCelsius(input);
+    setCTemp(result); setFTemp(input);
   }
 
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-      </fieldset>
-    );
-  }
+  return (
+    <>
+      <TemperatureInput
+        scale="celsius"
+        temp = {CTemp}
+        onTemperatureChange={handleCelsiusChange}
+      />
+      <TemperatureInput
+        scale="fahrenheit"
+        temp = {FTemp}
+        onTemperatureChange={handleFahrenheitChange} 
+      />
+      <BoilingVerdict celsius={CTemp}/>
+    </>
+  );
 }
-
-class Converter extends Component {
-  constructor(props) {
-    super(props);
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = {temperature: '', scale: 'c'};
-  }
-
-  handleCelsiusChange(temperature) {
-    this.setState({scale: 'c', temperature});
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({scale: 'f', temperature});
-  }
-
-  render() {
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
-    return (
-      <div>
-        <TemperatureInput
-          scale="c"
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange} 
-        />
-        <TemperatureInput
-          scale="f"
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange} 
-        />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)} 
-        />
-      </div>
-    );
-  }
-}
-
-export default Converter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Converter from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Converter />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<Converter />);
 reportWebVitals();
